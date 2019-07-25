24183764dbd51acc62884e0b0f51568f6e8ba8b9
openthc <code@openthc.com>
2019-07-25 09:41:12 +0000

    update routes, add some scaffolding

--- webroot/front.php
+++ webroot/front.php
@@ -3,9 +3,6 @@
  * OpenTHC Auth Front Controller
  */
 
-use Edoceo\Radix;
-use Edoceo\Radix\Session;
-
 // We may start with an error code from the PHP interpreter
 $e0 = error_get_last();
 $t0 = microtime(true);
@@ -20,15 +17,14 @@ header('X-XSS-Protection: 1; mode=block');
 require_once(dirname(dirname(__FILE__)) . '/boot.php');
 
 $cfg = array();
-$cfg = array('debug' => true);
+$cfg = array('debug' => false);
 $app = new \OpenTHC\App($cfg);
 
 // Authentication
 $app->group('/auth', function() {
 
-	$this->get('', 'App\Controller\Auth\Open');
+	// $this->get('', 'App\Controller\Auth\Open');
 	$this->get('/open', 'App\Controller\Auth\Open');
-
 	// $this->get('/back', 'App\Controller\Auth\Back');
 	$this->get('/fail', 'App\Controller\Auth\Fail');
 	$this->get('/ping', 'App\Controller\Auth\Ping');
@@ -36,6 +32,18 @@ $app->group('/auth', function() {
 	$this->get('/init', 'App\Controller\Auth\Init');
 
 })->add('OpenTHC\Middleware\Session');
+
+$app->group('/oauth2', function() {
+
+	$this->post('/token', 'App\Controller\oAuth2\Token');
+
+	$this->get('/authorize', 'App\Controller\oAuth2\Authorize');
+	$this->get('/permit', 'App\Controller\oAuth2\Accept');
+	$this->get('/reject', 'App\Controller\oAuth2\Reject');
+
+	$this->get('/profile', 'App\Controller\oAuth2\Profile');
+
+});
 
 
 $ret = $app->run();
