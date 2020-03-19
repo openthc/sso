c5140cd59cba034bb325ba8def2ecef2d68791e7
openthc <code@openthc.com>
2020-03-19 18:48:47 +0000

    update test config

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-# Test the SSO
+# OpenTHC Test Runner
 #
 
 set -o errexit
@@ -8,14 +8,22 @@ set -o nounset
 
 f=$(readlink -f "$0")
 d=$(dirname "$f")
+dt=$(date)
+dts=$(date +%Y%m%d-%H%M%S)
 
 cd "$d"
 
+out_path="../webroot/test-output"
+if [ ! -d "$out_path" ]
+then
+	mkdir "$out_path"
+fi
+
+
 #
 #
-(../vendor/bin/phpunit 2>&1 \
-	| tee ../webroot/test-output/output.txt \
-	) || true
+../vendor/bin/phpunit "$@" 2>&1 | tee "$out_path/output.txt"
+note=$(tail -n1 "$out_path/output.txt")
 
 #
 # Get Transform
@@ -26,39 +34,8 @@ fi
 
 xsltproc \
 	--nomkdir \
-	--output "../webroot/test-output/phpunit.html" \
+	--output "$out_path/output.html" \
 	phpunit-report.xsl \
-	../webroot/test-output/phpunit.xml
+	"$out_path/output.xml"
 
-dt=$(date)
-
-cat > ../webroot/test-output/index.html <<HTML
-<html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="viewport" content="initial-scale=1, user-scalable=yes">
-<meta name="application-name" content="OpenTHC">
-<meta name="apple-mobile-web-app-title" content="OpenTHC">
-<meta name="msapplication-TileColor" content="#247420">
-<meta name="theme-color" content="#247420">
-<link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
-<title>Test Result</title>
-</head>
-<body>
-<div class="container">
-
-<h1>Test Results ${dt}</h1>
-
-<p>You can view the <a href="output.txt">raw script output</a>,
-or the <a href="phpunit.xml">Unit Test XML</a>
-which we've processed <small>(via XSL)</small> to <a href="phpunit.html">a pretty report</a>
-which is also in <a href="testdox.html">testdox format</a>.
-</p>
-
-</div>
-</body>
-</html>
-HTML
+sed "s/%TEST_DATE%/$dt/g; s/%TEST_NOTE%/$note/g;" ./report.html > ../webroot/test-output/index.html
