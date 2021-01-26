603596197e605065924deb18d4acf3fdc248767a
openthc <code@openthc.com>
2021-01-26 22:49:21 +0000

    Cleanup Once handler

--- lib/Controller/Auth/Once.php
+++ lib/Controller/Auth/Once.php
@@ -11,113 +11,71 @@ class Once extends \App\Controller\Base
 {
 	function __invoke($REQ, $RES, $ARG)
 	{
-		// Hash Links
-		// Should be using a different controller?
-		if (!empty($_GET['_'])) {
-
-			if (!preg_match('/^([\w\-]{32,128})$/i', $_GET['_'], $m)) {
-				return $RES->withJSON([
-					'data' => null,
-					'meta' => [ 'detail' => 'Invalid Request [CAO-022]' ]
-				], 400);
-			}
-
-			$dbc = $this->_container->DBC_AUTH;
-			$act = $dbc->fetchOne('SELECT meta FROM auth_context_ticket WHERE id = :t', [ ':t' => $_GET['_']]);
-			$act = json_decode($act, true);
-			$act['intent'] = $act['intent'] ?: $act['action'];
-			switch ($act['intent']) {
-				case 'email-verify':
-					$arg = [
-						'action' => 'email-verify-save',
-						'contact' => $act['contact'],
-					];
-					$arg = json_encode($arg);
-					$arg = _encrypt($arg, $_SESSION['crypt-key']);
-
-					return $RES->withRedirect('/account/verify?_=' . $arg);
-
-				break;
-				case 'init':
-				case 'oauth-migrate':
-					// OK
-					return $RES->withJSON($act);
-			}
-
+		// Token Links
+		if (empty($_GET['_'])) {
+			__exit_text('Invalid Request [CAO-016]', 400);
 		}
 
-		if (empty($_GET['a'])) {
-			__exit_text('Invalid Request [CAO-047]', 400);
-		}
-
-
-		$data = $this->data;
-
-		// Well known actions
-		switch ($_GET['a']) {
-		case 'password-reset':
-
-			$data['Page'] = [ 'title' => 'Password Reset '];
-			$data['email'] = $_SESSION['email'];
-
-			$cfg = \OpenTHC\Config::get('google');
-			$data['Google']['recaptcha_public'] = $cfg['recaptcha-public'];
-
-			$file = 'page/auth/once-password-reset.html';
-
-			return $this->_container->view->render($RES, $file, $data);
-
-			break;
-		}
-
-		if (!preg_match('/^([\w\-]{32,128})$/i', $_GET['a'], $m)) {
-			__exit_text('Invalid Request [CAO-071]', 400);
+		if (!preg_match('/^([\w\-]{32,128})$/i', $_GET['_'], $m)) {
+			return $RES->withJSON([
+				'data' => null,
+				'meta' => [ 'detail' => 'Invalid Request [CAO-022]' ]
+			], 400);
 		}
 
 		$dbc_auth = $this->_container->DBC_AUTH;
-
-		$act = $dbc_auth->fetchRow('SELECT * FROM auth_context_ticket WHERE id = :t0', [ ':t0' => $_GET['a'] ]);
-		if (empty($act)) {
+		$tmp = $dbc_auth->fetchOne('SELECT meta FROM auth_context_ticket WHERE id = :t', [ ':t' => $_GET['_']]);
+		if (empty($tmp)) {
 			return $RES->withRedirect('/done?e=cao066');
 		}
+		$act = json_decode($tmp, true);
+		if (empty($act)) {
+			$dbc_auth->query('DELETE FROM auth_context_ticket WHERE id = :t0', [ ':t0' => $_GET['_'] ]);
+			return $RES->withRedirect('/done?e=cao077');
+		}
+
+		$act['intent'] = $act['intent'] ?: $act['action'];
+		switch ($act['intent']) {
+			case 'account-create':
+				return $this->accountCreate($RES, $act);
+				break;
+			case 'email-verify':
+
+				$arg = [
+					'action' => 'email-verify-save',
+					'contact' => $act['contact'],
+				];
+				$arg = json_encode($arg);
+				$arg = _encrypt($arg, $_SESSION['crypt-key']);
+
+				return $RES->withRedirect('/account/verify?_=' . $arg);
+
+				break;
+
+			case 'password-reset':
+
+				$val = [
+					'contact' => $act['contact'],
+				];
+				$val = json_encode($val);
+				$x = _encrypt($val, $_SESSION['crypt-key']);
+
+				return $RES->withRedirect('/account/password?_=' . $x);
+
+				break;
+
+			case 'init':
+			case 'oauth-migrate':
+				// OK
+				return $RES->withJSON($act);
+		}
+
+		$data = $this->data;
+
 		// if (strtotime($act['ts_expires']) < $_SERVER['REQUEST_TIME']) {
 			// $dbc->query('DELETE FROM auth_context_ticket WHERE id = :t0', $act['id']);
 			// __exit_html('<h1>Invalid Ticket [CAO#028]</h2><p>The link you followed has expired</p>', 400);
 		// }
-		$chk = json_decode($act['meta'], true);
-		if (empty($chk)) {
-			$dbc_auth->query('DELETE FROM auth_context_ticket WHERE id = :t0', [ ':t0' => $act['id'] ]);
-			return $RES->withRedirect('/done?e=cao077');
-		}
-		$act = $chk;
-
-		switch ($act['action']) {
-		case 'account-create':
-
-			return $this->accountCreate($RES, $act);
-
-		case 'email-verify':
-
-			$arg = [
-				'action' => 'email-verify-save',
-				'contact' => $act['contact'],
-			];
-			$arg = json_encode($arg);
-			$arg = _encrypt($arg, $_SESSION['crypt-key']);
-
-			return $RES->withRedirect('/account/verify?_=' . $arg);
-
-		case 'password-reset':
-
-			$val = [
-				'source' => 'email', // @deprecated use the parameter 'next'
-				'contact' => $act['contact'],
-			];
-			$val = json_encode($val);
-			$x = _encrypt($val, $_SESSION['crypt-key']);
-
-			return $RES->withRedirect('/account/password?_=' . $x);
-		}
 
 		$data['Page']['title'] = 'Error';
 		$RES = $this->_container->view->render($RES, 'page/done.html', $data);
