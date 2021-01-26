0d625d5e87086cfde5e8b7e77e62df6540209c2f
openthc <code@openthc.com>
2021-01-26 23:26:59 +0000

    cleanup shut

--- lib/Controller/Auth/Shut.php
+++ lib/Controller/Auth/Shut.php
@@ -9,21 +9,24 @@ class Shut extends \OpenTHC\Controller\Auth\Shut
 {
 	function __invoke($REQ, $RES, $ARG)
 	{
-		$RES = parent::__invoke($REQ, $RES, $ARG);
-
-		if (200 == $RES->getStatusCode()) {
-
-			$file = 'page/done.html';
-
-			$data = [];
-			$data['Page'] = [ 'title' => 'Session Closed' ];
-			$data['body'] = '<p>Your session has been closed</p><p>';
-			$data['foot'] = '<a class="btn btn-outline-secondary" href="/auth/open">Sign In Again</a>';
-
-			$RES = $this->_container->response;
-			$RES = $this->_container->view->render($RES, $file, $data);
-
+		// If parent class gives error we should do something smarter
+		$resX = parent::__invoke($REQ, $RES, $ARG);
+		if (200 != $resX->getStatusCode()) {
+			return $resX;
 		}
+
+		if (!empty($_GET['r'])) {
+			return $RES->withRedirect($_GET['r']);
+		}
+
+		$file = 'page/done.html';
+
+		$data = [];
+		$data['Page'] = [ 'title' => 'Session Closed' ];
+		$data['body'] = '<p>Your session has been closed</p><p>';
+		$data['foot'] = '<a class="btn btn-outline-secondary" href="/auth/open">Sign In Again</a>';
+
+		$RES = $this->_container->view->render($RES, $file, $data);
 
 		return $RES;
 
