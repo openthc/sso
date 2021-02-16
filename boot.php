204f47d50c372665ed9978c8821f2ab65303d750
djbusby <code@edoceo.com>
2021-02-16 18:52:42 +0000

    use common/lib/err

--- boot.php
+++ boot.php
@@ -11,4 +11,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 openlog('openthc-sso', LOG_ODELAY|LOG_PID, LOG_LOCAL0);
 
 require_once(APP_ROOT . '/vendor/autoload.php');
-require_once(APP_ROOT . '/lib/err.php');
+
+_error_handler_init();
+
+\OpenTHC\Config::init(__DIR__);
