66668feb5983c69d4357a41a3167a908a784e9bc
openthc <code@openthc.com>
2021-11-16 02:11:10 +0000

    update base data hostnames

--- etc/sql/90-data.sql
+++ etc/sql/90-data.sql
@@ -3,7 +3,7 @@
 --
 
 INSERT INTO auth_company (id, name) VALUES ('018NY6XC00C0MPANY000000000', '-system-');
-INSERT INTO auth_contact (id, username, password) values ('018NY6XC00C0NTACT000000000', 'root@openthc.com', ('LOCK:' || md5(random()::text)::text));
+INSERT INTO auth_contact (id, username, password) values ('018NY6XC00C0NTACT000000000', '-system-', ('LOCK:' || md5(random()::text)::text));
 
 INSERT INTO auth_company_contact (company_id, contact_id) VALUES ('018NY6XC00C0MPANY000000000', '018NY6XC00C0NTACT000000000');
 
@@ -19,6 +19,13 @@ INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'menu', 'On-lin
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'p2p', 'Peer-to-Peer Communications Tools');
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'pipe', 'CRE Data Pipe');
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'pos', 'Point of Sale');
+
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'sso.openthc.local', 'sso.openthc.local', 'sso.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'cre.openthc.local', 'cre.openthc.local', 'cre.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'app.openthc.local', 'app.openthc.local', 'app.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'lab.openthc.local', 'lab.openthc.local', 'lab.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'pos.openthc.local', 'pos.openthc.local', 'pos.openthc.local-secret');
+
 
 INSERT INTO acl_object_action (obj, act) VALUES ('company', 'search');
 INSERT INTO acl_object_action (obj, act) VALUES ('company', 'create');
