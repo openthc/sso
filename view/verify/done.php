8432688f68d4bde506e61893874ae74c2db73a63
openthc <code@openthc.com>
2023-03-06 22:42:45 +0000

    update links

--- view/verify/done.php
+++ view/verify/done.php
@@ -11,13 +11,13 @@
 <h1 class="card-header"><?= $data['Page']['title'] ?></h1>
 
 <div class="card-body">
-	<p>We have recieved your request, and will contact you shortly with the next steps.</p>
-	<p>You may get started using the <a href="">Demo environment</a>.</p>
+	<p>We have recieved your request and will contact you shortly with the next steps.</p>
+	<p>You may want to checkout the <a href="https://openthc.com/demo">Demo Environment</a>.</p>
 </div>
 
 <div class="card-footer">
-	<a class="btn btn-primary" href="" tabindex="1">Launch Demo <i class="far fa-external-link"></i></a>
-	<a class="btn btn-outline-danger" href="https://openthc.com/help" tabindex="2" style="float: right;">Get Help <i class="fas fa-life-ring"></i></a>
+	<a class="btn btn-primary" href="https://openthc.com/demo" tabindex="1">Browse Demo <i class="far fa-external-link"></i></a>
+	<a class="btn btn-outline-danger" href="https://openthc.com/help" tabindex="2" target="_blank" style="float: right;">Get Help <i class="fas fa-life-ring"></i></a>
 </div>
 
 </div>
