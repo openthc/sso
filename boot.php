f3e278b3bed4ed9a052b0418fc7205550121323c
openthc <code@openthc.com>
2023-04-24 14:42:53 +0000

    Use ENV

--- boot.php
+++ boot.php
@@ -14,8 +14,11 @@ openlog('openthc-sso', LOG_ODELAY|LOG_PID, LOG_LOCAL0);
 
 require_once(APP_ROOT . '/vendor/autoload.php');
 
-_error_handler_init();
-
 if ( ! \OpenTHC\Config::init(APP_ROOT) ) {
 	_exit_html_fail('<h1>Invalid Application Configuration [ALB-035]</h1>', 500);
 }
+
+define('APP_BASE', getenv('OPENTHC_SERVICE_BASE') ?: rtrim(\OpenTHC\Config::get('openthc/sso/base'), '/'));
+define('APP_ULID', getenv('OPENTHC_SERVICE_ID')   ?: \OpenTHC\Config::get('openthc/sso/id')              );
+
+_error_handler_init();
