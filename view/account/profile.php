5444b08e61f6f09919a3818ad410eb7c00a91921
openthc <code@openthc.com>
2023-04-25 16:45:40 +0000

    use config

--- view/account/profile.php
+++ view/account/profile.php
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MIT
  */
 
+$dir_base = rtrim(\OpenTHC\Config::get('openthc/dir/base'), '/');
+
 ?>
 
 
@@ -63,7 +65,7 @@ if ($data['company_list']) {
 			<?php
 			foreach ($data['company_list'] as $c) {
 			?>
-				<h3><a href="https://directory.openthc.com/company/<?= $c['id'] ?>" target="_blank"><?= h($c['name']) ?></a></h3>
+				<h3><a href="<?= sprintf('%s/company/%s', $dir_base, $c['id']) ?>" target="_blank"><?= h($c['name']) ?></a></h3>
 			<?php
 			}
 			?>
