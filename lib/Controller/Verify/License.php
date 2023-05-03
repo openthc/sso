aa7d8257205af84c5887c6843774e89971b2f7e1
openthc <code@openthc.com>
2023-05-03 18:17:41 +0000

    add license verify

--- lib/Controller/Verify/License.php
+++ lib/Controller/Verify/License.php
@@ -7,10 +7,44 @@
 
 namespace OpenTHC\SSO\Controller\Verify;
 
-class License extends \OpenTHC\SSO\Controller\Base
+class License extends \OpenTHC\SSO\Controller\Verify\Base
 {
+	/**
+	 *
+	 */
 	function __invoke($REQ, $RES, $ARG)
 	{
-		__exit_text('Not Implemented', 501);
+		$data = $this->data;
+		$data['Page']['title'] = 'Verify License';
+
+		$act = $this->loadTicket();
+
+		return $RES->write( $this->render('verify/license.php', $data) );
+
+	}
+
+	/**
+	 *
+	 */
+	function post($REQ, $RES, $ARG)
+	{
+		var_dump($_POST);
+
+		$LR0 = [
+			'code' => $_POST['license-code'],
+			'type' => $_POST['license-type'],
+			'name' => ($_POST['company-name'] ?: $act['contact']['email']),
+			'iso3166' => $act['iso3166'],
+	];
+
+// $RES = $RES->withAttribute('License', $LR0);
+// syslog(LOG_NOTICE, )
+
+
+		$_SESSION['verify']['license']['done'] = true;
+
+		return $RES->withRedirect(sprintf('/verify?_=%s', $_GET['_']));
+
+
 	}
 }
--- view/verify/license.php
+++ view/verify/license.php
@@ -14,18 +14,121 @@
 	<h1 class="card-header"><?= $data['Page']['title'] ?></h1>
 	<div class="card-body">
 
-	<div class="form-group">
-		<label>License</label>
-		<input autocomplete="off" class="form-control" id="contact-phone" inputmode="tel" name="contact-phone" placeholder="+1 206 555 1212" required type="tel" value="">
+	<p>All accounts in OpenTHC must be linked to a regulatory license.</p>
+	<p>If your region or industry sector does not issue a separate license from a typical business license, you may leave this blank.</p>
+
+	<div class="mt-4">
+		<label>License Name:</label>
+		<input autocomplete="off"
+			autofocus
+			class="form-control"
+			id="license-name"
+			name="licnese-name"
+			placeholder="License Name"
+			required
+			value="<?= __h($data['company']['name']) ?>">
+	</div>
+
+	<div class="mt-4">
+		<label>License Code:</label>
+		<input
+			autocomplete="off"
+			class="form-control"
+			id="license-code"
+			name="license-code"
+			placeholder="License Code or Number"
+			tabindex="1"
+			type="text"
+			value=""
+		>
+	</div>
+
+	<div class="mt-4">
+		<label>License Type:</label>
+		<select
+			autocomplete="off"
+			class="form-control"
+			id="license-type"
+			name="license-type"
+			tabindex="1"
+			type="text"
+			value=""
+		>
+			<option>Grower</option>
+			<option>Grower+Processor</option>
+			<option>Processor</option>
+			<option>Carrier</option>
+			<option>Laboratory</option>
+			<option>Retail</option>
+		</select>
+	</div>
+
+	<div class="mt-4">
+		<label>License Address:</label>
+		<input
+			autocomplete="off"
+			class="form-control"
+			id="license-code"
+			name="license-code"
+			placeholder="Full License Address"
+			tabindex="1"
+			type="text"
+			value=""
+		>
+	</div>
+
+	<div class="mt-4">
+		<label>License Phone:</label>
+		<input
+			autocomplete="off"
+			class="form-control"
+			id="license-code"
+			name="license-code"
+			placeholder="License Phone"
+			tabindex="1"
+			type="text"
+			value="<?= __h($data['license']['email']) ?>"
+		>
+	</div>
+
+	<div class="mt-4">
+		<label>License Email:</label>
+		<input
+			autocomplete="off"
+			class="form-control"
+			id="license-code"
+			name="license-code"
+			placeholder="License Email"
+			tabindex="1"
+			type="text"
+			value="<?= __h($data['company']['email']) ?>"
+		>
 	</div>
 
 	</div>
 
-	<div class="card-footer r">
-		<button class="btn btn-primary" name="a" type="submit" value="contact-next">Save <i class="icon icon-arrow-right"></i></button>
+	<div class="card-footer">
+		<button class="btn btn-primary" name="a" tabindex="1" type="submit" value="license-request">Save <i class="icon icon-arrow-right"></i></button>
+		<button class="btn btn-outline-secondary" id="btn-license-skip" name="a" tabindex="2" type="submit" value="license-skip">Skip <i class="icon icon-arrow-right"></i></button>
+
 	</div>
 
 	</div>
 
 </div>
 </form>
+
+
+<script>
+(function() {
+
+	var B = document.querySelector('#btn-license-skip');
+	B.addEventListener('click', function() {
+		var node_list = document.querySelectorAll('input[required]');
+		node_list.forEach(function(n) {
+			n.removeAttribute('required');
+		});
+	});
+
+})();
+</script>
