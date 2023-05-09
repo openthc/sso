b965d575201457faf5d61feecb37424d69ef2876
openthc <code@openthc.com>
2023-05-09 02:52:59 +0000

    stub the company join

---
+++ lib/Controller/Company/Join.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Join a Company
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace OpenTHC\SSO\Controller\Company;
+
+class Join extends \OpenTHC\SSO\Controller\Base
+{
+	/**
+	 *
+	 */
+	function __invoke($REQ, $RES, $ARG)
+	{
+		_exit_html_fail('<h1>Not Implemented</h1>', 501);
+	}
+
+}
