649169bae22d5be65e4259749c926b089c5a2072
openthc <code@openthc.com>
2023-05-29 20:47:11 +0000

    improve /done handlers

--- lib/Controller/Done.php
+++ lib/Controller/Done.php
@@ -9,42 +9,32 @@ namespace OpenTHC\SSO\Controller;
 
 class Done extends \OpenTHC\SSO\Controller\Base
 {
+	/**
+	 *
+	 */
 	function __invoke($REQ, $RES, $ARG)
 	{
-		$file = 'page/done.html';
-
 		$data = $this->data;
 		$data['Page'] = [];
 		$data['Page']['title'] = 'Done';
 
 		if (!empty($_GET['e'])) {
+
+			$data['error_code'] = $_GET['e'];
+
 			switch ($_GET['e']) {
 				case 'CAC-065':
 					$data['Page']['title'] = 'Account Error';
 					$data['warn'] = 'You have already created an account, sign in to that one';
 					$data['foot'] = '<div class="r"><a class="btn btn-outline-primary" href="/auth/open">Sign In <i class="icon icon-arrow-right"></i></a></div>';
-				break;
+					break;
 				case 'CAC-111':
 					$data['Page']['title'] = 'Account Confirmation';
 					$data['info'] = 'Please check your email to confirm your account.';
-					$data['body'] = '<p>We have just sent you an email, with the next steps.<p><p>You will need to confirm your request through a link in that message.</p><p>Maybe you want to read more about regulations?</p><div><a class="btn btn-outline-success" href="https://openthc.com/intro">Introduction to Track and Trace <i class="icon icon-arrow-right"></i></a></div>';
+					$data['body'] = '<p>We have just sent you an email, with the next steps.<p><p>You will need to confirm your request through a link in that message and then complete the verification process.</p>';
+					// <p>Maybe you want to read more about regulations?</p>
+					// <a class="btn btn-outline-success" href="https://openthc.com/intro">Introduction to Track and Trace <i class="icon icon-arrow-right"></i></a>
 					break;
-				case 'CAC-217':
-					$data['Page']['title'] = 'Check Your Email';
-					$data['warn'] = 'Possible Email Communications Issue';
-					$data['info'] = 'Please check your email to confirm your account.';
-					$data['body'] = '<p>We have just sent you an email, with the next steps.<p><p>You will need to confirm your request through a link in that message.</p><p>Maybe you want to read more about regulations?</p><div><a class="btn btn-outline-success" href="https://openthc.com/intro">Introduction to Track and Trace <i class="icon icon-arrow-right"></i></a></div>';
-					break;
-				case 'CAO-066':
-					$data['Page']['title'] = 'Error';
-					$data['fail'] = 'The link you followed is not valid';
-				break;
-				case 'CAO-073':
-					$data['Page']['title'] = 'Account Confirmed';
-					$data['info'] = 'Account Confirmed';
-					$data['body'] = '<p>Thank you, your email has been verified and your account request confirmed.</p><p>Next, you will need to set a password.</p>';
-					$data['foot'] = sprintf('<div class="r"><a class="btn btn-outline-success" href="/account/password?_=%s">Set Password <i class="icon icon-arrow-right"></i></a></div>', $_GET['_']);
-				break;
 				case 'CAO-100':
 					$data['Page']['title'] = 'Check Your Inbox';
 					$data['body'] = '<p>If the email address submitted was valid and has an account then an email should arrive shortly with password reset instructions.</p><p>Check your mailbox (or SPAM folder) for this message and follow the steps indicated.</p><hr><p>Contact <em><a href="mailto:help@openthc.com">help@openthc.com</a></em> if you need additional assistance</p>';
@@ -57,12 +47,13 @@ class Done extends \OpenTHC\SSO\Controller\Base
 				case 'CAV-228':
 					$data['Page']['title'] = 'Email Verification';
 					$data['body'] = '<div class="alert alert-success">Check Your Inbox!</div><p>Your your inbox for a message from us, there is a link we want you to click.</p>';
-				break;
+					break;
 				case 'CAV-255':
-				case 'CAC-190':
 					$data['Page']['title'] = 'Email Verification';
 					$data['body'] = '<div class="alert alert-danger">Email Verification Send Failure [CAV-255]</div><p>Please contact support</p>';
-				break;
+					break;
+					// return $RES->write( $this->render('done-x.php', $data) );
+					// $data['body'] =
 			}
 		}
 
--- view/done.php
+++ view/done.php
@@ -14,6 +14,17 @@ if ( ! empty($data['error_code'])) {
 		case 'CAO-040':
 			$data['fail'] = 'Invalid Request, Token Expired or Invalid';
 			break;
+		case 'CVM-130':
+			$data['Page']['title'] = 'Verification Complete';
+			$data['body'] = <<<HTML
+			<h2 class="alert alert-success">Account Pending Activation.</h2>
+			<p>You will soon receive an activation email which will complete the account creation process.</p>
+			HTML;
+			$data['foot'] = <<<HTML
+			<a class="btn btn-primary" href="https://openthc.com/demo" tabindex="1">Browse Demo <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+			<a class="btn btn-outline-danger" href="https://openthc.com/help" tabindex="2" target="_blank" style="float: right;">Get Help <i class="fas fa-life-ring"></i></a>
+			HTML;
+			break;
 		default:
 			$data['Page']['title'] = sprintf('Error: %s', $data['error_code']);
 	}
@@ -47,7 +58,6 @@ if ( ! empty($data['error_code'])) {
 			$sso_origin = OPENTHC_SERVICE_ORIGIN;
 			echo sprintf('<hr><div class="alert alert-warning">Auth: <a href="%s/auth/once?_=%s">SSO/auth/once</a></div>', $sso_origin, $_GET['t']);
 		}
-
 
 		?>
 
