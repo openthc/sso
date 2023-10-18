40cfc3dcc270ede665f3268b7e5633ccf8bd013d
openthc <code@openthc.com>
2023-10-18 17:07:37 +0000

    drop Modernizr dep

--- view/auth/open.php
+++ view/auth/open.php
@@ -70,9 +70,16 @@ if ( ! empty($svc)) {
 </form>
 
 
-<script src="https://cdn.openthc.com/modernizr/2.8.3/modernizr.js" integrity="sha256-0rguYS0qgS6L4qVzANq4kjxPLtvnp5nn2nB5G1lWRv4=" crossorigin="anonymous"></script>
 <script>
 document.querySelector('#js-enabled').value = 1;
-document.querySelector('#date-input-enabled').value = (Modernizr.inputtypes.date + 0);
-document.querySelector('#time-input-enabled').value = (Modernizr.inputtypes.time + 0);
+document.querySelector('#date-input-enabled').value = (function() {
+	var node = document.createElement('input');
+	node.type = 'date';
+	return ('date' === node.type)
+})();
+document.querySelector('#time-input-enabled').value = (function() {
+	var node = document.createElement('input');
+	node.type = 'time';
+	return ('time' === node.type)
+})();
 </script>
