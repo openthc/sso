841637d572f945ea0f072e0107323a78bd536ef4
openthc <code@openthc.com>
2024-04-25 16:32:18 +0000

    add test mode helper

--- boot.php
+++ boot.php
@@ -24,4 +24,32 @@ if ( ! \OpenTHC\Config::init(APP_ROOT) ) {
 define('OPENTHC_SERVICE_ID', \OpenTHC\Config::get('openthc/sso/id'));
 define('OPENTHC_SERVICE_ORIGIN', \OpenTHC\Config::get('openthc/sso/origin'));
 
-_error_handler_init();
+// _error_handler_init();
+
+/**
+ * Checks if we're in TEST mode
+ */
+function is_test_mode() {
+
+	// Cookie Check
+	if ( ! empty($_COOKIE['openthc-test'])) {
+		$sk1 = $_COOKIE['openthc-test'];
+		// Cookie Pattern Check
+		if (preg_match('/^[\w\-\+\/]{32,64}$/', $sk1)) {
+			// Config Check
+			$sk0 = \OpenTHC\Config::get('openthc/sso/test/sk');
+			if ( ! empty($sk0)) {
+				// Time Check
+				// if ($sk0 >= 48 hours old) {
+				// 	return false;
+				// }
+				if ($sk0 === $sk1) {
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+
+}
