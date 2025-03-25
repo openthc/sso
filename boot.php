9b5b08741e96459fbd6b01dd52497356f90cc8cb
openthc <code@openthc.com>
2025-03-25 23:48:15 +0000

    update deps; bump version

--- boot.php
+++ boot.php
@@ -9,7 +9,7 @@
 // declare(strict_types=1);
 
 define('APP_ROOT', __DIR__);
-define('APP_BUILD', '420.25.071');
+define('APP_BUILD', '420.25.084');
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
 
