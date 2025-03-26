94234c702288ebe235399016630d3e9760993224
edoceo <code@edoceo.com>
2025-03-25 17:02:34 -0700

    Cleanup Make Helper

--- make.sh
+++ make.sh
@@ -8,65 +8,28 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
-BIN_SELF=$(readlink -f "$0")
-APP_ROOT=$(dirname "$BIN_SELF")
+APP_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-action="${1:-}"
-shift
+cd "$APP_ROOT"
 
-case "$action" in
-# Install or Update the System
-install|update)
+composer install --no-ansi --no-dev --no-progress --quiet --classmap-authoritative
 
-	composer update --no-ansi --no-dev --no-progress --quiet --classmap-authoritative
-	npm install
+npm install --no-audit --no-fund --package-lock-only --silent
 
-	./make.sh vendor-web
+php <<PHP
+<?php
+require_once(__DIR__ . '/boot.php');
 
-	;;
+\OpenTHC\Make::install_bootstrap();
+\OpenTHC\Make::install_fontawesome();
+\OpenTHC\Make::install_jquery();
 
-# Get the CSS and JS Assets
-vendor-web)
+PHP
 
-	# lodash
-	# mkdir -p webroot/vendor/lodash/
-	# cp node_modules/lodash/lodash.min.js webroot/vendor/lodash/
+# lodash
+# mkdir -p webroot/vendor/lodash/
+# cp node_modules/lodash/lodash.min.js webroot/vendor/lodash/
 
-	# jquery
-	mkdir -p webroot/vendor/jquery/
-	cp node_modules/jquery/dist/jquery.min.js webroot/vendor/jquery/
-	cp node_modules/jquery/dist/jquery.min.map webroot/vendor/jquery/
-
-	cp node_modules/jquery-ui/dist/jquery-ui.min.js webroot/vendor/jquery/
-	cp node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css webroot/vendor/jquery/
-
-	# bootstrap
-	mkdir -p webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/css/bootstrap.min.css           webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/css/bootstrap.min.css.map       webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js      webroot/vendor/bootstrap/
-	cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map  webroot/vendor/bootstrap/
-
-	# htmx
-	# mkdir -p webroot/vendor/htmx
-	# cp node_modules/htmx.org/dist/htmx.min.js webroot/vendor/htmx/
-
-	# font awesome
-	mkdir -p webroot/vendor/fontawesome/css webroot/vendor/fontawesome/webfonts
-	cp node_modules/@fortawesome/fontawesome-free/css/all.min.css webroot/vendor/fontawesome/css/
-	cp node_modules/@fortawesome/fontawesome-free/webfonts/* webroot/vendor/fontawesome/webfonts/
-
-	;;
-
-# Help, the default target
-*)
-
-	echo
-	echo "You must supply a make command"
-	echo
-	awk '/^# [A-Z].+/ { h=$0 }; /^[a-z]+.+\)/ { printf " \033[0;49;31m%-15s\033[0m%s\n", gensub(/\)$/, "", 1, $$1), h }' "$BIN_SELF" |sort
-	echo
-
-	;;
-
-esac
+# htmx
+# mkdir -p webroot/vendor/htmx
+# cp node_modules/htmx.org/dist/htmx.min.js webroot/vendor/htmx/
