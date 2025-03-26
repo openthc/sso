fec3784fc2f4896e65a0fc33fdcdd74e9c911693
edoceo <code@edoceo.com>
2025-03-26 14:38:10 -0700

    fix IDs

--- etc/sql/90-data.sql
+++ etc/sql/90-data.sql
@@ -2,10 +2,10 @@
 -- Default Scopes
 --
 
-INSERT INTO auth_company (id, name) VALUES ('018NY6XC00C0MPANY000000000', '-system-');
+INSERT INTO auth_company (id, name) VALUES ('010PENTHCXC0MPANY00000R00T', '-system-');
 INSERT INTO auth_contact (id, username, password) values ('018NY6XC00C0NTACT000000000', '-system-', ('LOCK:' || md5(random()::text)::text));
 
-INSERT INTO auth_company_contact (company_id, contact_id) VALUES ('018NY6XC00C0MPANY000000000', '018NY6XC00C0NTACT000000000');
+INSERT INTO auth_company_contact (company_id, contact_id) VALUES ('010PENTHCXC0MPANY00000R00T', '018NY6XC00C0NTACT000000000');
 
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'ops', 'OPS');
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'b2b', 'Business to Business Sales Platform');
@@ -19,8 +19,8 @@ INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'p2p', 'Peer-to
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'pipe', 'CRE Data Pipe');
 INSERT INTO auth_context (id, code, name) VALUES (ulid_create(), 'pos', 'Point of Sale');
 
-INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'sso.openthc.local', 'sso.openthc.local', 'sso.openthc.local-secret');
-INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'cre.openthc.local', 'cre.openthc.local', 'cre.openthc.local-secret');
-INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'app.openthc.local', 'app.openthc.local', 'app.openthc.local-secret');
-INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'lab.openthc.local', 'lab.openthc.local', 'lab.openthc.local-secret');
-INSERT INTO auth_service (company_id, name, code, hash) VALUES ('018NY6XC00C0MPANY000000000', 'pos.openthc.local', 'pos.openthc.local', 'pos.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('010PENTHCXC0MPANY00000R00T', 'sso.openthc.local', 'sso.openthc.local', 'sso.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('010PENTHCXC0MPANY00000R00T', 'cre.openthc.local', 'cre.openthc.local', 'cre.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('010PENTHCXC0MPANY00000R00T', 'app.openthc.local', 'app.openthc.local', 'app.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('010PENTHCXC0MPANY00000R00T', 'lab.openthc.local', 'lab.openthc.local', 'lab.openthc.local-secret');
+INSERT INTO auth_service (company_id, name, code, hash) VALUES ('010PENTHCXC0MPANY00000R00T', 'pos.openthc.local', 'pos.openthc.local', 'pos.openthc.local-secret');
