0350952c23e29c799292b2ceb781abc0114d88c7
openthc <code@openthc.com>
2026-08-27 20:11:49 +0000

    use .sh helper

--- make.sh
+++ make.sh
@@ -18,19 +18,8 @@ composer install --no-ansi --no-progress --classmap-authoritative
 
 npm install --no-audit --no-fund
 
-php <<PHP
-<?php
-define('APP_ROOT', __DIR__);
-require_once(APP_ROOT . '/vendor/autoload.php');
-\OpenTHC\Make::install_bootstrap();
-\OpenTHC\Make::install_fontawesome();
-\OpenTHC\Make::install_jquery();
-PHP
-
-# lodash
-# mkdir -p webroot/vendor/lodash/
-# cp node_modules/lodash/lodash.min.js webroot/vendor/lodash/
-
-# htmx
-# mkdir -p webroot/vendor/htmx
-# cp node_modules/htmx.org/dist/htmx.min.js webroot/vendor/htmx/
+vendor/openthc/common/lib/make.sh install_bootstrap
+vendor/openthc/common/lib/make.sh install_fontawesome
+vendor/openthc/common/lib/make.sh install_jquery
+# vendor/openthc/common/lib/make.sh install_lodash
+# vendor/openthc/common/lib/make.sh install_htmx
